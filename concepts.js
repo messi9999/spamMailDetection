53e32068251ed2199a7d304a55657ccfff45804a
SWExpertBH0915 <brandonhilbrands915@gmail.com>
2022-05-28 03:26:12 -0500

    hello

--- concepts.js
+++ concepts.js
@@ -1,175 +1,237 @@
 
-import React from "react";
-import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
-import Header from "../components/Header";
-import { useState } from "react";
-import { Button, Form, Modal, Table } from "react-bootstrap";
-import axios from "axios";
-import { useEffect } from "react";
-import { ReactComponent as EditIcon } from "../img/edit-button-svgrepo-com.svg";
-import { ReactComponent as DeleteIcon } from "../img/icons8-delete.svg";
+import React, { useState, useRef, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-const BASE_URL = process.env.REACT_APP_BASEURL;
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
+import { isEmail } from "validator";
 
-export default function Adminboard() {
-  const [show, setShow] = useState(false);
-  const [userID, setUserID] = useState();
+import { register } from "../actions/auth";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useNavigate } from "react-router-dom";
+
+import { ReactComponent as BugleIcaon } from "../img/icon_logo.svg";
+
+const required = (value) => {
+  if (!value) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This field is required!
+      </div>
+    );
+  }
+};
+
+const validEmail = (value) => {
+  if (!isEmail(value)) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This is not a valid email.
+      </div>
+    );
+  }
+};
+
+const vusername = (value) => {
+  if (value.length < 3 || value.length > 20) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        The username must be between 3 and 20 characters.
+      </div>
+    );
+  }
+};
+
+const vpassword = (value) => {
+  if (value.length < 6 || value.length > 40) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        The password must be between 6 and 40 characters.
+      </div>
+    );
+  }
+};
+
+const Register = () => {
+  const form = useRef();
+  const checkBtn = useRef();
+
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
-  const [expireDate, setExpireDate] = useState();
+  const [password, setPassword] = useState("");
+  const [successful, setSuccessful] = useState(false);
+  const [confirmpassword, setConfirmpassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
-  const handleClose = () => setShow(false);
+  const roles = ["user"];
 
-  const [users, setUsers] = useState([]);
+  const { message } = useSelector((state) => state.message);
 
-  const handleEdit = (userId, userName, userEmail, userExpireDate) => {
-    // Logic to handle the edit action for the user with the specified ID
-    setUserID(userId);
-    setUsername(userName);
-    setEmail(userEmail);
-    setExpireDate(userExpireDate);
+  const dispatch = useDispatch();
 
-    setShow(true);
-    console.log(`Editing user with ID: ${userID}`);
+  const navigate = useNavigate();
+
+  const onChangeUsername = (e) => {
+    const username = e.target.value;
+    setUsername(username);
+  };
+
+  const onChangeEmail = (e) => {
+    const email = e.target.value;
+    setEmail(email);
+  };
+
+  const onChangePassword = (e) => {
+    const password = e.target.value;
+    setPassword(password);
+  };
+
+  const onChangeConPassword = (e) => {
+    e.preventDefault();
+    setConfirmpassword(e.target.value);
   };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(BASE_URL + "/all"); // Replace '/api/users' with your API endpoint
-        setUsers(response.data); // Assuming the response contains an array of users
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
+    if (password !== confirmpassword) {
+      setPasswordError("Passwords do not match");
+    } else {
+      // Password and confirm password match, do something
+      setPasswordError("");
+    }
+  }, [password, confirmpassword]);
 
-    fetchData();
-  }, []);
+  const handleRegister = (e) => {
+    e.preventDefault();
 
-  const handleDelete = async (userId) => {
-    // Logic to handle the delete action for the user with the specified ID
-    const res = await axios.delete(BASE_URL + `/delete/${userId}`);
-    console.log(res);
-    window.location.reload();
+    setSuccessful(false);
+
+    form.current.validateAll();
+    if (passwordError) {
+      alert("Please confirm your password");
+    } else if (checkBtn.current.context._errors.length === 0) {
+      dispatch(register(username, email, password, roles))
+        .then(() => {
+          setSuccessful(true);
+        })
+        .catch(() => {
+          setSuccessful(false);
+        });
+    }
   };
 
-  const handleSaveChanges = async () => {
-    const body = {
-      username: username,
-      email: email,
-      expireDate: expireDate
-    };
-    const res1 = await axios.put(BASE_URL + `/update/${userID}`, body);
-    console.log(res1);
-    setShow(false);
-    window.location.reload();
-  };
-
-  const { user: currentUser } = useSelector((state) => state.auth);
-  if (!currentUser) {
-    return <Navigate to="/login" />;
-  }
-  if (!currentUser.roles[0]) {
-    return <Navigate to="/login" />;
-  }
   return (
-    <div className="home-main bg-black mb-0 bg-gradient py-3">
-      <div style={{ height: "18vh" }}>
-        <Header />
-        <div style={{ marginTop: "20vh" }}>
-          <div>
-            <h2 className="d-flex justify-content-start align-content-center">
-              Admin
-            </h2>
-          </div>
-          <div>
-            <Table responsive="sm">
-              <thead>
-                <tr>
-                  <th>ID</th>
-                  <th>Username</th>
-                  <th>Email</th>
-                  <th>ExpireDate</th>
-                </tr>
-              </thead>
-              <tbody>
-                {users.map((user) => (
-                  <tr key={user.id}>
-                    <td>{user.id}</td>
-                    <td>{user.username}</td>
-                    <td>{user.email}</td>
-                    <td>{user.expireDate}</td>
-                    <td className="d-flex justify-content-start align-content-center">
-                      <div
-                        onClick={() =>
-                          handleEdit(
-                            user.id,
-                            user.username,
-                            user.email,
-                            user.expireDate
-                          )
-                        }
-                      >
-                        <EditIcon style={{ width: "25px", height: "auto" }} />
-                      </div>
-                      <div onClick={() => handleDelete(user.id)}>
-                        <DeleteIcon style={{ width: "25px", height: "auto" }} />
-                      </div>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
-          </div>
+    <div className="col-md-12">
+      <div className="card card-container text-white rounded-4">
+        <label className="fs-3 fw-bolder text-center mb-2">
+          Welcome to the Bugle AI Club!
+        </label>
+        <label className="fs-5 fw-semibold text-center mb-3">
+          Thanks for joining us, please finish your account setup below
+        </label>
+        <div className="profile-img-card">
+          <BugleIcaon />
         </div>
+
+        <Form onSubmit={handleRegister} ref={form}>
+          {!successful && (
+            <div>
+              <div className="form-group mb-3">
+                <label htmlFor="username">Create a Username</label>
+                <Input
+                  type="text"
+                  className="form-control"
+                  name="username"
+                  value={username}
+                  onChange={onChangeUsername}
+                  validations={[required, vusername]}
+                />
+              </div>
+
+              <div className="form-group mb-3">
+                <label htmlFor="email">Confirm your Email</label>
+                <Input
+                  type="email"
+                  className="form-control"
+                  name="email"
+                  value={email}
+                  onChange={onChangeEmail}
+                  validations={[required, validEmail]}
+                />
+              </div>
+
+              <div className="form-group mb-3">
+                <label htmlFor="password">Create a Password</label>
+                <Input
+                  type="password"
+                  className="form-control"
+                  name="password"
+                  value={password}
+                  onChange={onChangePassword}
+                  validations={[required, vpassword]}
+                />
+              </div>
+
+              <div className="form-group mb-3">
+                <label htmlFor="confirmpassword">Confirm Password</label>
+                <Input
+                  type="password"
+                  className="form-control"
+                  name="confirmpassword"
+                  value={confirmpassword}
+                  onChange={onChangeConPassword}
+                />
+                <div className="text-center w-100 p-2">
+                  {passwordError && (
+                    <span className="text-center bg-danger bg-opacity-50 mt-5 w-100 p-2">
+                      {passwordError}
+                    </span>
+                  )}
+                </div>
+              </div>
+
+              <div className="form-group mt-3">
+                <button className="btn btn-primary btn-block float-end">
+                  Sign Up
+                </button>
+                <button
+                  className="btn btn-sm btn-white border-0 text-decoration-underline mt-4 text-white"
+                  onClick={() => {
+                    navigate("/login");
+                  }}
+                >
+                  Already have an account? Login here
+                </button>
+              </div>
+            </div>
+          )}
+
+          {message && (
+            <div className="form-group">
+              <div
+                className={
+                  successful ? "alert alert-success" : "alert alert-danger"
+                }
+                role="alert"
+              >
+                {message}
+              </div>
+              <button
+                className="btn btn-sm btn-white bg-black text-white border-0 text-decoration-underline mt-4"
+                onClick={navigate("/login")}
+              >
+                Signin now
+              </button>
+            </div>
+          )}
+          <CheckButton style={{ display: "none" }} ref={checkBtn} />
+        </Form>
       </div>
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group>
-              <Form.Label>{userID}</Form.Label>
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Username</Form.Label>
-              <Form.Control
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>ExpireDate</Form.Label>
-              <Form.Control
-                type="date"
-                value={expireDate}
-                onChange={(e) => setExpireDate(e.target.value)}
-              />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleSaveChanges}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
     </div>
   );
-}
+};
+
+export default Register;
 
             
