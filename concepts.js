e5d5d7bb6b81a1145fd6c5bc290ed0598a2c6f20
SWExpertBH0915 <brandonhilbrands915@gmail.com>
2023-02-10 02:36:24 -0600

    include bash script

--- concepts.js
+++ concepts.js
@@ -1,26 +1,14 @@
 
-import React, { useState } from "react";
-import video from "../video/demo.mp4";
-import Header from "./Header";
-const VideoPlayer = () => {
+import React from "react";
+
+export default function Confirm() {
+  const urlParams = new URLSearchParams(window.location.search);
+  console.log(urlParams);
   return (
-    <div className="home-main bg-black mb-0 bg-gradient py-3">
-      <div style={{ height: "18vh" }}>
-        <Header />
-      </div>
-      <div
-        className="vh-100"
-        style={{ display: "flex", justifyContent: "center" }}
-      >
-        <video width="65%" height="55%" controls>
-          <source src={video} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
+    <div>
+      <button>Confirm</button>
     </div>
   );
-};
-
-export default VideoPlayer;
+}
 
             
