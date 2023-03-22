b95cd0a785b26891ee9a0a76b8698db24fb52546
SWExpertBH0915 <brandonhilbrands915@gmail.com>
2023-03-22 03:51:53 -0500

    main function

--- concepts.js
+++ concepts.js
@@ -1,35 +1,25 @@
 
-import React from "react";
-import Header from "../components/Header";
+import React, { useState } from 'react';
 
-export default function ContactUs() {
-  return (
-    <div>
-      <div style={{ height: "18vh" }}>
-        <div className="home-main bg-black mb-0 bg-gradient py-3">
-          <Header />
-        </div>
-        <div className="bg-black" style={{ height: "110vh" }}>
-          <div className="text-white text-center d-flex flex-column justify-content-center align-content-center w-100 pt-5">
-            <div className="pb-5">
-              <h1>Contact Us</h1>
-            </div>
-            <div>
-              <h4>Email: phil@robotpigeon.co</h4>
-            </div>
-            <div>
-              <label className="fs-4">Bugle AI</label>
-            </div>
-            <div>
-              <label className="fs-4">
-                Colonial House Beverley East Yorkshire UK HU17 0LS
-              </label>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+
+const LoadingButton = ({ onClick, children }) => {
+    const [loading, setLoading] = useState(false);
+    const [completed, setCompleted] = useState(false);
+  
+    const handleClick = async () => {
+      setLoading(true);
+      await onClick();
+      setLoading(false);
+      setCompleted(true);
+    };
+  
+    return (
+      <button className="btn-submit" onClick={handleClick} disabled={loading}>
+        {loading ? 'Loading...' : completed ? 'Submit' : children}
+      </button>
+    );
+  };
+
+export default LoadingButton;
 
             
