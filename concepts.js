d5a88b097a30716892103e485c87aedd593ac3e4
SWExpertBH0915 <brandonhilbrands915@gmail.com>
2023-05-06 04:01:04 -0500

    replace

--- concepts.js
+++ concepts.js
@@ -1,26 +1,25 @@
 
-import React, { useState } from "react";
-import video from "../video/demo.mp4";
-import Header from "./Header";
-const VideoPlayer = () => {
-  return (
-    <div className="home-main bg-black mb-0 bg-gradient py-3">
-      <div style={{ height: "18vh" }}>
-        <Header />
-      </div>
-      <div
-        className="vh-100"
-        style={{ display: "flex", justifyContent: "center" }}
-      >
-        <video width="65%" height="55%" controls>
-          <source src={video} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
-    </div>
-  );
-};
+import React, { useState } from 'react';
 
-export default VideoPlayer;
+
+const LoadingButton = ({ onClick, children }) => {
+    const [loading, setLoading] = useState(false);
+    const [completed, setCompleted] = useState(false);
+  
+    const handleClick = async () => {
+      setLoading(true);
+      await onClick();
+      setLoading(false);
+      setCompleted(true);
+    };
+  
+    return (
+      <button className="btn-submit" onClick={handleClick} disabled={loading}>
+        {loading ? 'Loading...' : completed ? 'Submit' : children}
+      </button>
+    );
+  };
+
+export default LoadingButton;
 
             
