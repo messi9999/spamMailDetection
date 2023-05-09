674441056731a3a9887b5f5b8b00ed5e8b1d89d7
SWExpertBH0915 <brandonhilbrands915@gmail.com>
2023-05-09 04:01:39 -0500

    readme update

--- concepts.js
+++ concepts.js
@@ -1,26 +1,35 @@
 
-import React, { useState } from "react";
-import video from "../video/demo.mp4";
-import Header from "./Header";
-const VideoPlayer = () => {
+import React from "react";
+import Header from "../components/Header";
+
+export default function ContactUs() {
   return (
-    <div className="home-main bg-black mb-0 bg-gradient py-3">
+    <div>
       <div style={{ height: "18vh" }}>
-        <Header />
-      </div>
-      <div
-        className="vh-100"
-        style={{ display: "flex", justifyContent: "center" }}
-      >
-        <video width="65%" height="55%" controls>
-          <source src={video} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+        <div className="home-main bg-black mb-0 bg-gradient py-3">
+          <Header />
+        </div>
+        <div className="bg-black" style={{ height: "110vh" }}>
+          <div className="text-white text-center d-flex flex-column justify-content-center align-content-center w-100 pt-5">
+            <div className="pb-5">
+              <h1>Contact Us</h1>
+            </div>
+            <div>
+              <h4>Email: phil@robotpigeon.co</h4>
+            </div>
+            <div>
+              <label className="fs-4">Bugle AI</label>
+            </div>
+            <div>
+              <label className="fs-4">
+                Colonial House Beverley East Yorkshire UK HU17 0LS
+              </label>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
-};
-
-export default VideoPlayer;
+}
 
             
