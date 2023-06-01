7787a09b3fe5562f5aae2485ca203bc132d4c3db
SWExpertBH0915 <brandonhilbrands915@gmail.com>
2023-06-01 04:06:17 -0500

    functional component

--- concepts.js
+++ concepts.js
@@ -1,25 +1,14 @@
 
-import React, { useState } from 'react';
+import React from "react";
 
-
-const LoadingButton = ({ onClick, children }) => {
-    const [loading, setLoading] = useState(false);
-    const [completed, setCompleted] = useState(false);
-  
-    const handleClick = async () => {
-      setLoading(true);
-      await onClick();
-      setLoading(false);
-      setCompleted(true);
-    };
-  
-    return (
-      <button className="btn-submit" onClick={handleClick} disabled={loading}>
-        {loading ? 'Loading...' : completed ? 'Submit' : children}
-      </button>
-    );
-  };
-
-export default LoadingButton;
+export default function Confirm() {
+  const urlParams = new URLSearchParams(window.location.search);
+  console.log(urlParams);
+  return (
+    <div>
+      <button>Confirm</button>
+    </div>
+  );
+}
 
             
