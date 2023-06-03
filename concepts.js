c39e9fd633f31bc9b384c0abcfc5ed90cf3d49f1
SWExpertBH0915 <brandonhilbrands915@gmail.com>
2023-06-03 04:06:27 -0500

    readme update

--- concepts.js
+++ concepts.js
@@ -1,14 +1,26 @@
 
-import React from "react";
-
-export default function Confirm() {
-  const urlParams = new URLSearchParams(window.location.search);
-  console.log(urlParams);
+import React, { useState } from "react";
+import video from "../video/demo.mp4";
+import Header from "./Header";
+const VideoPlayer = () => {
   return (
-    <div>
-      <button>Confirm</button>
+    <div className="home-main bg-black mb-0 bg-gradient py-3">
+      <div style={{ height: "18vh" }}>
+        <Header />
+      </div>
+      <div
+        className="vh-100"
+        style={{ display: "flex", justifyContent: "center" }}
+      >
+        <video width="65%" height="55%" controls>
+          <source src={video} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
     </div>
   );
-}
+};
+
+export default VideoPlayer;
 
             
